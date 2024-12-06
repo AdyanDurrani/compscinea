c440dad89e116bd40d717c441b9d400d566913e9
Adyan <ad@Adyans-MacBook-Air.local>
2024-12-06 09:42:42 +0000

    1.0

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
- 
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Camera not being read")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("can not read camera.")
+        break
+
+    cv.imshow("frame", frame)
+
+    if cv.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+cv.destroyAllWindows()
